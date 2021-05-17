fdcea40a417eaacca363f57c743e73cf54d24ec7
19036308 <19036308@myrp.edu.sg>
2021-05-17 16:12:47 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19036308/demodatabasecrud/EditActivity.java
@@ -0,0 +1,60 @@
+package sg.edu.rp.c346.id19036308.demodatabasecrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EditActivity extends AppCompatActivity {
+
+    TextView tvID;
+    EditText etContent;
+    Button btnUpdate, btnDelete;
+    Note data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        //initialise the variables with UI here
+        etContent = findViewById(R.id.etContent2);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+        tvID = findViewById(R.id.tvID);
+
+        Intent i = getIntent();
+        data = (Note) i.getSerializableExtra("data");
+
+        tvID.setText("ID: " + data.getId());
+        etContent.setText(data.getNoteContent());
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                data.setNoteContent(etContent.getText().toString());
+                dbh.updateNote(data);
+                setResult( RESULT_OK, i);
+                dbh.close();
+                finish();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                dbh.deleteNote(data.getId());
+                setResult( RESULT_OK, i);
+                dbh.close();
+                finish();
+            }
+        });
+
+    }
+}
